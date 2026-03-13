b76649ce07d121fd71b92fa6640545a7b06ca624
prashant133 <dsrprasant@gmail.com>
2026-03-13 21:59:39 +0545

    file model created

---
+++ src/modules/file/file.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const fileSchema = mongoose.Schema(
+  {
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    originalName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    storedName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    mimeType: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const File = mongoose.model("File", fileSchema);
+
+module.exports = File;
